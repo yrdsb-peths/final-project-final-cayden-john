12e6bb2dacfc8695d75bdc6a62f6b002fe259d67
caydench <349581918@gapps.yrdsb.ca>
2025-05-20 14:05:16 -0400

    Add Martin

---
+++ Martin.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Martin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Martin extends Actor
+{
+    GreenfootImage martin = new GreenfootImage("images/png/Idle (1).png");
+    public void act()
+    {
+        
+    }
+    
+    public Martin() {
+       setImage(martin);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Martin martin = new Martin();
+        addObject(martin, 300, 200);
     }
 }
