0ce7972a840156438bb54fcbc1816b254bcdee3b
caydench <349581918@gapps.yrdsb.ca>
2025-05-23 14:23:33 -0400

    change martin facing

--- Martin.java
+++ Martin.java
@@ -8,23 +8,54 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Martin extends Actor
 {
-    GreenfootImage martin = new GreenfootImage("images/png/Idle (1).png");
+    GreenfootImage[] idleRight = new GreenfootImage[6];
+    GreenfootImage[] idleLeft = new GreenfootImage[6];
+    SimpleTimer animationTimer = new SimpleTimer();
+    String facing = "right";
+    
     public void act()
     {
-<<<<<<< Updated upstream
-        
-=======
         if(Greenfoot.isKeyDown("left")) {
             move(-5);
+            facing = "left";
         } else if(Greenfoot.isKeyDown("right")) {
             move(5);
+            facing = "right";
         }
+        
         animateMartin();
->>>>>>> Stashed changes
     }
     
     public Martin() {
-       setImage(martin);
-       martin.scale(155, 158);
+        for(int i = 0; i < idleRight.length; i++) {
+           idleRight[i] = new GreenfootImage("images/Idle Hero/idle" + i + ".png");
+           idleRight[i].scale(400, 400);
+        }
+        
+        for(int i = 0; i < idleLeft.length; i++) {
+           idleLeft[i] = new GreenfootImage("images/Idle Hero/idle" + i + ".png");
+           idleLeft[i].mirrorHorizontally();
+           idleLeft[i].scale(400, 400);
+        }
+       
+        animationTimer.mark();
+       
+        setImage(idleRight[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateMartin() {
+        if(animationTimer.millisElapsed() < 100) {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right")) {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        } else {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -4,11 +4,7 @@ public class MyWorld extends World {
     public MyWorld() {
         super(1127, 557, 1);
         Martin martin = new Martin();
-<<<<<<< Updated upstream
-        addObject(martin, 300, 200);
-=======
         addObject(martin, 100, 500);
->>>>>>> Stashed changes
         setBackground("images/Cave.png");
     }
 }
